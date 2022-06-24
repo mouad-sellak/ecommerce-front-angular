6f41068ecc37704d682573c77f630fd1b442383d
Mouad Sellak <mouaadsellak123@gmail.com>
2022-06-24 16:39:57 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule  } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+import {Routes, RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+
+const routes:Routes=[
+  {path:"products/:id",component:ProductsComponent},
+  {path:'',redirectTo:'products/3',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
